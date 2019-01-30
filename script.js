2978029750b71e9a77087d93ce906a7c70cd7c60
magerrrr <kiruha24@mail.ru>
2019-01-31 00:38:08 +0300

    git init

---
+++ script.js
@@ -0,0 +1,10 @@
+var tarif = 10 //тариф за час игры
+
+sum = 20 // вводим сумму
+totalSum = sum - (sum * 15/100) //сумма со скидкой
+
+HoursForGame = Sum/tarif
+TimeInMinuts = HoursForGame*60// подсчет времени согласно сумме
+
+
+Время: TimeInMinuts c отсчетом
