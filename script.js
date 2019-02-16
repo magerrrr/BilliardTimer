a5277312a74f87c24885abb4a42dd0c818116bb5
magerrrr <kiruha24@mail.ru>
2019-02-16 22:09:42 +0300

    added TO DO

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//TO DO: Set tarifPerHour depending on the day of the week
+
 var tarifPerHour = prompt("Введите тариф в час", 10);
 var discount = prompt("Введите скидку", 15);
 var pricePerHour = calculateDiscount(tarifPerHour, discount);
@@ -6,35 +8,29 @@ var currentTime;
 var timerId;
 var price;
 var isButtonStop = false;
-
 function calculateDiscount(tarif, disc){
 return tarif - (tarif*disc/100);
 }
-
 function resetStartTime() { // НАЧАТЬ ИГРУ
 	timerId = setInterval(getMoney,1); // счетчик
 	isButtonStop = false;
 	startPlayingTime = new Date();
 	return startPlayingTime.getTime();
 }
-
 function getCurrentTime(){
 	currentTime = new Date();
 	return currentTime.getTime();
 }
-
 function getTime(){
 	var a = getCurrentTime() - startPlayingTime;
 	return a;
 }
-
 function getMoney(){
 	var price = getTime()/3600000*pricePerHour;
 	rub = Math.floor(price);
 	cop = (price - rub)*100;
 	document.getElementById('result').innerHTML = rub + " Руб. " + cop.toFixed(0) + " Коп.";
 }
-
 function stopTime(){
 	if(!isButtonStop){
 		isButtonStop = true;
