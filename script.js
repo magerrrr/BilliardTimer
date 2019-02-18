e726bb2c185fce1871d246540196151b5accc966
magerrrr <kiruha24@mail.ru>
2019-02-18 23:55:23 +0300

    added div with start-time from variable startPlayingTime

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//TO DO: add div with start-time drom variable startPlayingTime
-
 var tarifPerHour;
-var discount = prompt("Введите скидку", 15);
+var discount = 15;
 var pricePerHour;
 var startPlayingTime;
 var currentTime;
 var timerId;
+var timerTime;
 var price;
 var isButtonStop = false;
+var test;
 
 function calculateDiscount(tarif, disc){
 return tarif - (tarif*disc/100);
@@ -15,37 +15,45 @@ return tarif - (tarif*disc/100);
 
 function resetStartTime() { // НАЧАТЬ ИГРУ
 
-	timerId = setInterval(getMoney,1); // счетчик
+	timerId = setInterval(getMoney,1000); // счетчик
 	isButtonStop = false;
 	startPlayingTime = new Date();
 	getCurrentTarif();
 	pricePerHour = calculateDiscount(tarifPerHour, discount);
+	
+	timerTime = setInterval("displayPlayingTime(getTime())",1000);
 	return startPlayingTime.getTime();
 }
+
 function getCurrentTime(){
 	currentTime = new Date();
 	return currentTime.getTime();
 }
+
 function getTime(){
 	var a = getCurrentTime() - startPlayingTime;
 	return a;
 }
+
 function getMoney(){
 	var price = getTime()/3600000*pricePerHour;
 	rub = Math.floor(price);
 	cop = (price - rub)*100;
 	document.getElementById('result').innerHTML = rub + " Руб. " + cop.toFixed(0) + " Коп.";
 }
+
 function stopTime(){
 	if(!isButtonStop){
 		isButtonStop = true;
 		price = getTime()/3600000*pricePerHour;
 		document.getElementById('result').innerHTML = "К оплате: " + price.toFixed(2) + " руб";
 		clearInterval(timerId); // сброс счетчика
+		clearInterval(timerTime);
 	} else {
 		document.getElementById('result').innerHTML = "К оплате: " + price.toFixed(2) + " руб";
 	}
 }
+
 function getCurrentTarif(){
 	var currentDay = startPlayingTime.getDay();
 	var currentHourOfDay = startPlayingTime.getHours();
@@ -76,3 +84,22 @@ function getCurrentTarif(){
 		    break;
 	}
 }
+
+function displayPlayingTime(millisec) {
+        var seconds = (millisec / 1000).toFixed(0);
+        var minutes = Math.floor(seconds / 60);
+        var hours = "";
+        if (minutes > 59) {
+            hours = Math.floor(minutes / 60);
+            hours = (hours >= 10) ? hours : "0" + hours;
+            minutes = minutes - (hours * 60);
+            minutes = (minutes >= 10) ? minutes : "0" + minutes;
+        }
+
+        seconds = Math.floor(seconds % 60);
+        seconds = (seconds >= 10) ? seconds : "0" + seconds;
+        if (hours != "") {
+            return hours + ":" + minutes + ":" + seconds;
+        }
+        document.getElementById('time').innerHTML = minutes + ":" + seconds;
+    }
